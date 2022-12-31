bb6953e897123b81ddaf8844eb07923aa7bdf792
Lamadrid-Mike <mikealamadrid@hotmail.com>
2022-12-30 20:27:00 -0500

    Home page created

---
+++ scripts.js
@@ -0,0 +1,25 @@
+const products = [
+  {
+    name: "Cheese",
+    weight: "200g",
+    type: "Block",
+    price: 10,
+  },
+  {
+    name: "Milk",
+    weight: "200ml",
+    price: 5,
+  },
+  {
+    name: "Tomato",
+    weight: "100g",
+    price: 2.75,
+  },
+  {
+    name: "Pineapple",
+    weight: "500g",
+    price: 3.25,
+  },
+];
+
+console.log(products);
