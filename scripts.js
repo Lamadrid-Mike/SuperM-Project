7bec8380ec3e533b32effd4d0c7c5c00d54d3460
Lamadrid-Mike <mikealamadrid@hotmail.com>
2023-01-01 12:14:14 -0500

    Poducts page created

--- scripts.js
+++ scripts.js
@@ -4,22 +4,44 @@ const products = [
     weight: "200g",
     type: "Block",
     price: 10,
+    src: "/cheese.png",
   },
   {
     name: "Milk",
     weight: "200ml",
     price: 5,
+    src: "/milk.png",
   },
   {
     name: "Tomato",
     weight: "100g",
     price: 2.75,
+    src: "/tomato.png",
   },
   {
     name: "Pineapple",
     weight: "500g",
     price: 3.25,
+    src: "/pineapple.png",
   },
 ];
 
 console.log(products);
+
+const items = document.querySelector(".productsContainer");
+const cartValue = Number(document.querySelector(".cartValue").innerHTML);
+
+const loadItems = function () {
+  products.forEach(function (el, i) {
+    let div = ` <div class="item${i + 1} itemsLayout">
+    <img src="${el.src}" width="150" height="150" />
+    <div>
+      <p>${el.name}</p>
+      <p>${el.weight}</p>
+    </div>
+    <button class="itemsBtn">$${el.price}</button>
+  </div>`;
+    items.insertAdjacentHTML("beforeend", div);
+  });
+};
+loadItems();
