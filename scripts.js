e484d8d8cf5a0d4230eac2b481a6ea5aad435329
Lamadrid-Mike <mikealamadrid@hotmail.com>
2023-04-11 20:28:07 -0400

    js file edited

--- scripts.js
+++ scripts.js
@@ -26,12 +26,12 @@ const products = [
   },
 ];
 
-const product = document.querySelector(".productsContainer");
+const productsContainer = document.querySelector(".productsContainer");
+const hiddenCartContainer = document.querySelector(".cartContainer");
 const cartValue = document.querySelector(".cartValue");
-const cartContainer = document.querySelector(".cartContainer");
 
-export let cache = JSON.parse(localStorage.getItem("cart-data"));
 let productsAdded = [];
+let cartContainerNames = [];
 let closeBtn;
 
 const loadProducts = function () {
@@ -42,29 +42,28 @@ const loadProducts = function () {
     const img = document.createElement("img");
     const outerDiv = document.createElement("div");
     const button = document.createElement("button");
-    const span = document.createElement("closeBtn");
+    const closeBtn = document.createElement("closeBtn");
     div.classList.add(`item${i + 1}`);
     div.classList.add("itemsLayout");
     div.appendChild(img).setAttribute("src", `${el.src}`);
     img.setAttribute("width", "120");
     img.setAttribute("height", "110");
     img.classList.add("centerDivs");
-    product
+    productsContainer
       .appendChild(div)
       .appendChild(outerDiv)
       .appendChild(p).textContent = `${el.name}`;
     outerDiv.classList.add("centerDivs");
     outerDiv.appendChild(info).textContent = `${el.weight}`;
     button.classList.add("itemsBtn");
-    button.setAttribute("id", `${i + 1}`);
+    button.setAttribute("id", `${i}`);
     button.setAttribute("type", "submit");
-    span.classList.add("centerDivs");
-    span.classList.add("closeBtn");
-    div.appendChild(span).textContent = "x";
+    closeBtn.classList.add("centerDivs");
+    closeBtn.classList.add("closeBtn");
+    closeBtn.setAttribute("id", `${i}`);
+    div.appendChild(closeBtn).textContent = "x";
     div.appendChild(button).textContent = `${el.price}`;
   });
-
-  const itemsBtn = Array.from(document.getElementsByTagName("button"));
 
   class Cart {
     constructor(name, price, quantity = 0, weight) {
@@ -75,70 +74,56 @@ const loadProducts = function () {
     }
   }
 
-  const showCloseBtn = function (el) {
-    closeBtn = Array.from(document.getElementsByTagName("closeBtn"));
-    let match = el.name;
-    products.find((el, index) => {
-      if (el.name === match) {
-        closeBtn[index].classList.add("showBtn");
-        const p = document.createElement("p");
-        p.setAttribute("id", `${products[index].name}`);
-        cartContainer.classList.add("cartContainerFocus");
-        cartContainer.appendChild(p).textContent = el.name;
-      }
-    });
+  const displayCartContainer = (productName) => {
+    if (!cartContainerNames.includes(productName)) {
+      cartContainerNames.push(productName);
+      const p = document.createElement("p");
+      p.setAttribute("id", productName);
+      p.innerHTML = productName;
+      hiddenCartContainer.classList.add("cartContainerFocus");
+      hiddenCartContainer.append(p);
+      console.log(cartContainerNames);
+    }
   };
 
-  if (cache !== null) {
-    cache.forEach((el) => {
-      productsAdded.push(el);
-      cartValue.textContent = cache.length;
-      showCloseBtn(el);
-    });
-  }
+  const cartContainerRemoval = (productName) => {
+    let selectedProduct = products[productName].name;
+    let filtered = cartContainerNames.filter((el) => el != selectedProduct);
+    let productToRemove = document.querySelector(`#${selectedProduct}`);
+    productToRemove.remove();
+    cartContainerNames = filtered;
+    displayCartValue();
+  };
 
-  // Adding products to cart function
-  itemsBtn.forEach((btn, index) =>
-    btn.addEventListener("click", function () {
-      if (!productsAdded.some((pro) => pro.name === products[index].name)) {
-        showCloseBtn(products[index]);
-        cartValue.textContent = productsAdded.length + 1;
-        productsAdded.push(
-          new Cart(
-            products[index].name,
-            products[index].price,
-            1,
-            products[index].weight
-          )
-        );
-        localStorage.setItem("cart-data", JSON.stringify(productsAdded));
-        productsAdded.forEach((el) => {
-          cartContainer.appendChild(p).textContent = el.name;
-        });
-      }
-    })
-  );
+  const showCloseBtn = (productBtn) => {
+    closeBtn = Array.from(document.getElementsByTagName("closeBtn"));
+    closeBtn[productBtn].classList.add("showBtn");
+  };
 
-  // Close btn function
-  closeBtn = Array.from(document.getElementsByTagName("closeBtn"));
-  closeBtn.forEach((btn, index) => {
-    btn.addEventListener("click", function () {
-      productsAdded.forEach((pro, position) => {
-        if (pro.name === products[index].name) {
-          productsAdded.splice(position, 1);
-          let nodeRemove = document.getElementById(`${products[index].name}`);
-          cartContainer.removeChild(nodeRemove);
-          localStorage.setItem("cart-data", JSON.stringify(productsAdded));
-        }
-      });
-      if (!productsAdded.some((pro) => pro.name === products[index].name)) {
-        closeBtn[index].classList.remove("showBtn");
-      }
-      productsAdded.length === 0
-        ? cartContainer.classList.remove("cartContainerFocus")
-        : "";
-      cartValue.textContent = productsAdded.length;
-    });
+  const hideCloseBtn = (productBtn) => {
+    closeBtn[productBtn].classList.remove("showBtn");
+  };
+
+  const displayCartValue = () => {
+    cartValue.innerHTML = cartContainerNames.length;
+    if (cartContainerNames.length === 0) {
+      hiddenCartContainer.classList.remove("cartContainerFocus");
+    }
+  };
+
+  productsContainer.addEventListener("click", function (e) {
+    let productId;
+    if (e.target.classList.contains("itemsBtn")) {
+      productId = e.target.getAttribute("id");
+      displayCartContainer(products[productId].name);
+      displayCartValue();
+      showCloseBtn(productId);
+    }
+    if (e.target.classList.contains("closeBtn")) {
+      productId = e.target.getAttribute("id");
+      hideCloseBtn(productId);
+      cartContainerRemoval(productId);
+    }
   });
 };
 
