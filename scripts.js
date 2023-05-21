0a123772eb6cf794be76239c4913ad5599b8075b
Mike Lamadrid <mikealamadrid@hotmail.com>
2023-05-20 20:07:21 -0400

    files edited

--- scripts.js
+++ scripts.js
@@ -65,107 +65,108 @@ const loadProducts = function () {
     div.appendChild(closeBtn).textContent = "x";
     div.appendChild(button).textContent = `${el.price}`;
   });
-};
-addEventListener("load", loadProducts);
 
-let storage = localStorage.getItem("productsAdded");
-let loadedData = JSON.parse(storage);
+  let storage = localStorage.getItem("productsAdded");
+  let loadedData = JSON.parse(storage);
 
-class Cart {
-  constructor(id, name, price, weight, quantity) {
-    (this.id = id),
-      (this.name = name),
-      (this.price = price),
-      (this.weight = weight),
-      (this.quantity = quantity);
-  }
-}
-
-const displayCartContainer = (productName) => {
-  cartContainerNames.push(productName);
-  const p = document.createElement("p");
-  p.setAttribute("id", productName);
-  p.innerHTML = productName;
-  hiddenCartContainer.classList.add("cartContainerFocus");
-  hiddenCartContainer.append(p);
-};
-
-const cartContainerRemoval = (productName) => {
-  let selectedProduct = products[productName].name;
-  let filtered = cartContainerNames.filter((el) => el != selectedProduct);
-  let productToRemove = document.querySelector(`#${selectedProduct}`);
-  productToRemove.remove();
-  cartContainerNames = filtered;
-  displayCartValue();
-};
-
-const showCloseBtn = (productBtn) => {
-  closeBtn = Array.from(document.getElementsByTagName("closeBtn"));
-  closeBtn[productBtn].classList.add("showBtn");
-};
-
-const hideCloseBtn = (productBtn) => {
-  closeBtn[productBtn].classList.remove("showBtn");
-};
-
-const displayCartValue = () => {
-  cartValue.innerHTML = cartContainerNames.length;
-  if (cartContainerNames.length === 0) {
-    hiddenCartContainer.classList.remove("cartContainerFocus");
-  }
-};
-
-const addToLocalStorage = (product) => {
-  localStorage.setItem("productsAdded", JSON.stringify(product));
-};
-
-if (loadedData !== null) {
-  cartValue.innerHTML = loadedData.length;
-  loadedData.forEach((el) => {
-    displayCartContainer(el.name);
-    showCloseBtn(el.id);
-    productsAdded.push(el);
-  });
-}
-
-productsContainer.addEventListener("click", function (e) {
-  let productId = e.target.getAttribute("id");
-  let productName = products[productId].name;
-  if (
-    e.target.classList.contains("itemsBtn") &&
-    !cartContainerNames.includes(productName)
-  ) {
-    quantity++;
-    displayCartContainer(productName);
-    showCloseBtn(productId);
-    displayCartValue();
-    productsAdded.push(
-      new Cart(
-        productId,
-        products[productId].name,
-        products[productId].price,
-        products[productId].weight,
-        quantity
-      )
-    );
-    quantity = 0;
-  } else {
-    for (let i = 0; i < productsAdded.length; i++) {
-      if (productsAdded[i].name === productName) {
-        productsAdded[i].quantity++;
-      }
+  class Cart {
+    constructor(id, name, price, weight, quantity) {
+      (this.id = id),
+        (this.name = name),
+        (this.price = price),
+        (this.weight = weight),
+        (this.quantity = quantity);
     }
   }
-  addToLocalStorage(productsAdded);
-  console.log(productsAdded);
 
-  if (e.target.classList.contains("closeBtn")) {
-    let productId = e.target.getAttribute("id");
-    hideCloseBtn(productId);
-    cartContainerRemoval(productId);
-    let filteredProducts = productsAdded.filter(
-      (el) => el.name !== productName
-    );
-    addToLocalStorage(filteredProducts);
+  const displayCartContainer = (productName) => {
+    cartContainerNames.push(productName);
+    const p = document.createElement("p");
+    p.setAttribute("id", productName);
+    p.innerHTML = productName;
+    hiddenCartContainer.classList.add("cartContainerFocus");
+    hiddenCartContainer.append(p);
+  };
+
+  const cartContainerRemoval = (productName) => {
+    let selectedProduct = products[productName].name;
+    let filtered = cartContainerNames.filter((el) => el != selectedProduct);
+    let productToRemove = document.querySelector(`#${selectedProduct}`);
+    productToRemove.remove();
+    cartContainerNames = filtered;
+    displayCartValue();
+  };
+
+  const showCloseBtn = (productBtn) => {
+    closeBtn = Array.from(document.getElementsByTagName("closeBtn"));
+    closeBtn[productBtn].classList.add("showBtn");
+  };
+
+  const hideCloseBtn = (productBtn) => {
+    closeBtn[productBtn].classList.remove("showBtn");
+  };
+
+  const displayCartValue = () => {
+    cartValue.innerHTML = cartContainerNames.length;
+    if (cartContainerNames.length === 0) {
+      hiddenCartContainer.classList.remove("cartContainerFocus");
+    }
+  };
+
+  const addToLocalStorage = (product) => {
+    localStorage.setItem("productsAdded", JSON.stringify(product));
+  };
+
+  if (loadedData !== null) {
+    cartValue.innerHTML = loadedData.length;
+    loadedData.forEach((el) => {
+      displayCartContainer(el.name);
+      showCloseBtn(el.id);
+      productsAdded.push(el);
+    });
   }
-});
+
+  productsContainer.addEventListener("click", function (e) {
+    let productId = e.target.getAttribute("id");
+    let productName = products[productId].name;
+    if (
+      e.target.classList.contains("itemsBtn") &&
+      !cartContainerNames.includes(productName)
+    ) {
+      quantity++;
+      displayCartContainer(productName);
+      showCloseBtn(productId);
+      displayCartValue();
+      productsAdded.push(
+        new Cart(
+          productId,
+          products[productId].name,
+          products[productId].price,
+          products[productId].weight,
+          quantity
+        )
+      );
+      quantity = 0;
+    } else {
+      for (let i = 0; i < productsAdded.length; i++) {
+        if (productsAdded[i].name === productName) {
+          productsAdded[i].quantity++;
+        }
+      }
+    }
+    addToLocalStorage(productsAdded);
+    console.log(productsAdded);
+
+    if (e.target.classList.contains("closeBtn")) {
+      let productId = e.target.getAttribute("id");
+      hideCloseBtn(productId);
+      cartContainerRemoval(productId);
+      let filteredProducts = productsAdded.filter(
+        (el) => el.name !== productName
+      );
+      addToLocalStorage(filteredProducts);
+    }
+  });
+};
+
+addEventListener("load", loadProducts);
